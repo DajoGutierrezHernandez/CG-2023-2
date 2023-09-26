f084fdbcd39bafc46cbae59677603bfbf8b041ff
DajoGutierrezHernandez <141768974+DajoGutierrezHernandez@users.noreply.github.com>
2023-09-26 15:44:00 -0500

    Actualización teclas
    q y e= movimiento en z                                                  j=rotación x
    a y d= movimiento en x                                                  i=rotación y
    w y s= movimiento en y                                                  l=rotación z

--- c02/threejs_ej03/cono.js
+++ c02/threejs_ej03/cono.js
@@ -9,9 +9,10 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.Geometry();
 const angulo1= Math.PI/4;
 const r=5;
+const h=7
 // Definir los vértices del prisma
 geometry.vertices.push(
- new THREE.Vector3(0, r, 0),//0
+ new THREE.Vector3(0, h, 0),//0
  new THREE.Vector3(r, 0, 0),//1
  new THREE.Vector3(r*Math.cos(Math.PI/4), 0, r*Math.sin(Math.PI/4)),//2
  new THREE.Vector3(0, 0, r),//3
@@ -37,17 +38,40 @@ geometry.faces.push(
 );
 // Calcular normales para las caras
 geometry.computeFaceNormals();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00,side: THREE.DoubleSide });
+
+//const loader=new THREE.TextureLoader();
+//const textura= loader.load("textura.jpeg");
+const material = new THREE.MeshBasicMaterial({  color: 0x00ff00  ,wireframe: false,side: THREE.DoubleSide });
 const customMesh = new THREE.Mesh(geometry, material);
 scene.add(customMesh);    
-camera.position.z = 10;
+camera.position.z = 15;
 camera.position.y = 3;
 
+document.addEventListener('keydown',(event)=>{
+    if(event.key==='a'||event.key==='A'){
+        customMesh.position.x-=0.1;
+    }else if(event.key==='d'||event.key==='D'){
+        customMesh.position.x+=0.1;
+    }else if(event.key==='w'||event.key==='W'){
+        customMesh.position.y+=0.1;
+    }else if(event.key==='s'||event.key==='S'){
+        customMesh.position.y-=0.1;
+    }else if(event.key==='q'||event.key==='Q'){
+        customMesh.position.z-=0.1;
+    }else if(event.key==='e'||event.key==='E'){
+        customMesh.position.z+=0.1;
+    }else if(event.key==='j'||event.key==='J'){
+        customMesh.rotation.x+=0.1;
+    }else if(event.key==='k'||event.key==='K'){
+        customMesh.rotation.y+=0.1;
+    }else if(event.key==='l'||event.key==='L'){
+        customMesh.rotation.z+=0.1;
+    }
+})
 
 function animate() {
  requestAnimationFrame(animate);
- customMesh.rotation.x+=0.01;
- customMesh.rotation.y+=0.01;
+
  renderer.render(scene, camera);
 }
 animate();
