c11579802c57e7ed2425df7a374f3fa1e57da99d
DajoGutierrezHernandez <141768974+DajoGutierrezHernandez@users.noreply.github.com>
2023-09-26 14:59:04 -0500

    Arreglo cono

--- c02/threejs_ej03/cono.js
+++ c02/threejs_ej03/cono.js
@@ -11,34 +11,33 @@ const angulo1= Math.PI/4;
 const r=5;
 // Definir los vértices del prisma
 geometry.vertices.push(
- new THREE.Vector3(0, r, 0),
- new THREE.Vector3(r, 0, 0),
- new THREE.Vector3(0, 0, r),
- new THREE.Vector3(-r, 0, 0),
- new THREE.Vector3(0, 0, -r),
- new THREE.Vector3(r*Math.cos(Math.PI/4), 0, r*Math.sin(Math.PI/4)),
- new THREE.Vector3(-r*Math.cos(Math.PI/4), 0, r*Math.sin(Math.PI/4)),
- new THREE.Vector3(-r*Math.cos(Math.PI/4), 0,-r*Math.sin(Math.PI/4)),
- new THREE.Vector3(r*Math.cos(Math.PI/4), 0, -r*Math.sin(Math.PI/4)),
+ new THREE.Vector3(0, r, 0),//0
+ new THREE.Vector3(r, 0, 0),//1
+ new THREE.Vector3(r*Math.cos(Math.PI/4), 0, r*Math.sin(Math.PI/4)),//2
+ new THREE.Vector3(0, 0, r),//3
+ new THREE.Vector3(-r*Math.cos(Math.PI/4), 0, r*Math.sin(Math.PI/4)),//4
+ new THREE.Vector3(-r, 0, 0),//5
+ new THREE.Vector3(-r*Math.cos(Math.PI/4), 0,-r*Math.sin(Math.PI/4)),//6
+ new THREE.Vector3(0, 0, -r),//7
+ new THREE.Vector3(r*Math.cos(Math.PI/4), 0, -r*Math.sin(Math.PI/4)),//8
+
+
 );
 // Definir las caras del prisma
 geometry.faces.push(
- new THREE.Face3(0, 1, 5),
- new THREE.Face3(0, 5, 4),
- new THREE.Face3(1, 2, 6),
- new THREE.Face3(1, 6, 5),
- new THREE.Face3(2, 3, 7),
- new THREE.Face3(2, 7, 6),
- new THREE.Face3(3, 0, 4),
- new THREE.Face3(3, 4, 7),
- new THREE.Face3(4, 5, 6),
- new THREE.Face3(4, 6, 7),
- new THREE.Face3(0, 3, 2),
- new THREE.Face3(0, 2, 1)
+ new THREE.Face3(0, 1, 2),
+ new THREE.Face3(0, 2, 3),
+ new THREE.Face3(0, 3, 4),
+ new THREE.Face3(0, 4, 5),
+ new THREE.Face3(0, 5, 6),
+ new THREE.Face3(0, 6, 7),
+ new THREE.Face3(0, 7, 8),
+ new THREE.Face3(0, 8, 1),
+ 
 );
 // Calcular normales para las caras
 geometry.computeFaceNormals();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00,wireframe: true });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00,side: THREE.DoubleSide });
 const customMesh = new THREE.Mesh(geometry, material);
 scene.add(customMesh);    
 camera.position.z = 10;
@@ -47,6 +46,8 @@ camera.position.y = 3;
 
 function animate() {
  requestAnimationFrame(animate);
+ customMesh.rotation.x+=0.01;
+ customMesh.rotation.y+=0.01;
  renderer.render(scene, camera);
 }
 animate();
