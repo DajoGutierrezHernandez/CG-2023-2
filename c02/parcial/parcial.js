7c06f42d72400498e78a7f3766ee50239b39132b
DajoGutierrezHernandez <141768974+DajoGutierrezHernandez@users.noreply.github.com>
2023-10-10 14:56:36 -0500

    Correcciones valores transformación
    Agregué comentarios y mejoré los valores para que cuando se cambie el lado, la figura se mantenga igual

--- c02/parcial/parcial.js
+++ c02/parcial/parcial.js
@@ -7,28 +7,28 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const caja1 = new THREE.Geometry();
 const caja2 = new THREE.Geometry();
-const caja3 = new THREE.Geometry();
-const lado=1;
-const escalado1=1;
-const escalado2=0.5;
-const escalado3=0.25;
+const caja3 = new THREE.Geometry();//crea 3 cubos utilizando Geometry
+const lado=1;//se asigna el lado del cubo
+const escalado1=1;//tamaño del escalado para el cubo1
+const escalado2=0.5;//tamaño del escalado para el cubo2
+const escalado3=0.25;//tamaño del escalado para el cubo3
 
-vertices(caja1,lado);
-vertices(caja2,lado);
-vertices(caja3,lado);
+vertices(caja1,lado);//se llama la función vertices para crear el cubo1
+vertices(caja2,lado);//se llama la función vertices para crear el cubo2
+vertices(caja3,lado);//se llama la función vertices para crear el cubo3
 
 const material1 = new THREE.MeshBasicMaterial({  color: 0x00ff00  ,wireframe: false,side: THREE.DoubleSide });
 const material2 = new THREE.MeshBasicMaterial({  color: 0x009933  ,wireframe: false,side: THREE.DoubleSide });
-const material3 = new THREE.MeshBasicMaterial({  color: 0xCC0033  ,wireframe: false,side: THREE.DoubleSide });
-const cubo1 = new THREE.Mesh(caja1, material1);
-const cubo2 = new THREE.Mesh(caja2, material2);
-const cubo3 = new THREE.Mesh(caja3, material3);
-scene.add(cubo1);
-scene.add(cubo2);
-scene.add(cubo3);
-trasformaciones(cubo1,escalado1,0);
-trasformaciones(cubo2,escalado2,(lado+escalado2));
-trasformaciones(cubo3,escalado3,lado+lado+escalado3);      
+const material3 = new THREE.MeshBasicMaterial({  color: 0xCC0033  ,wireframe: false,side: THREE.DoubleSide });//se crean 3 materiales con valores de colores distintos
+const cubo1 = new THREE.Mesh(caja1, material1);//se crea objeto/cubo1
+const cubo2 = new THREE.Mesh(caja2, material2);//se crea objeto/cubo2
+const cubo3 = new THREE.Mesh(caja3, material3);//se crea objeto/cubo3
+scene.add(cubo1);//Se añade cubo1 a la escena
+scene.add(cubo2);//Se añade cubo2 a la escena
+scene.add(cubo3);//Se añade cubo3 a la escena
+trasformaciones(cubo1,escalado1,0);//se implementa la función transformaciones para el cubo1
+trasformaciones(cubo2,escalado2,(lado+(lado*escalado2)));//se implementa la función transformaciones para el cubo2
+trasformaciones(cubo3,escalado3,lado+(2*lado*escalado2)+(lado*escalado3));      //se implementa la función transformaciones para el cubo3
 camera.position.z = 10;
 camera.position.y = 4;
 camera.position.x=2;
@@ -36,13 +36,12 @@ camera.position.x=2;
 const loader=new THREE.TextureLoader();
 loader.load('espada.jpeg',(texture)=>{
     material1.map=texture;
-})
+})//intento mapeo de texturas
 
 
 function trasformaciones(geo,escalar,posicion){
-    geo.scale.set(escalar,escalar,escalar);
-    geo.position.y+=posicion;
-
+    geo.scale.set(escalar,escalar,escalar);//escala el objeto utilizando el valor asignado
+    geo.position.y=posicion;//se mueve el objeto a la poscición ingresada
 }
 function vertices(geometria,l){
     geometria.vertices.push(
@@ -54,7 +53,7 @@ function vertices(geometria,l){
         new THREE.Vector3( l, l, -l),
         new THREE.Vector3( l, l, l),
         new THREE.Vector3(-l, l, l)
-       );
+       );//se definen los vertices del cubo en base al valor l (lado) ingresado
        geometria.faces.push(
         new THREE.Face3(0, 1, 5),
         new THREE.Face3(0, 5, 4),
@@ -68,7 +67,7 @@ function vertices(geometria,l){
         new THREE.Face3(4, 6, 7),
         new THREE.Face3(0, 3, 2),
         new THREE.Face3(0, 2, 1)
-       );
+       );//añade y procesa las caras del cubo
        geometria.computeFaceNormals();
 
 }
